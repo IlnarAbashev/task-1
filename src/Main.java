568a0edd8dca5d88aee94f4845918d4f14de789e
Ilnar Abashev <ilnarabashev@yandex.ru>
2023-03-12 16:41:00 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int Price = 5000;
+        int mile = 20;
+        int bonus;
+        bonus = Price / mile;
+
+        System.out.println(bonus);
+    }
+}
